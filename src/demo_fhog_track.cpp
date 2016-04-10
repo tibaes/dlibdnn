95b93ec8cf387724673f8e0ebcf36f0dcd956c96
Rafael H Tibaes (home) <r@fael.nl>
2016-04-10 17:08:04 -0300

    Working fake gun detection

--- src/demo_fhog_track.cpp
+++ src/demo_fhog_track.cpp
@@ -1,23 +1,23 @@
 // Rafael H. Tibaes !2016
 // r@fael.nl http://fael.nl
 
-
-#include <dlib/svm_threaded.h>
 #include <dlib/data_io.h>
-#include <dlib/image_processing.h>
+#include <dlib/dir_nav.h>
 #include <dlib/gui_widgets.h>
 #include <dlib/image_io.h>
-#include <dlib/dir_nav.h>
-#include <stdexcept>
-#include <iostream>
+#include <dlib/image_processing.h>
+#include <dlib/image_transforms.h>
+#include <dlib/svm_threaded.h>
 #include <fstream>
+#include <iostream>
+#include <stdexcept>
 
 using namespace dlib;
 using namespace std;
 
-int main(int argc, char** argv) {
-  const string detectorFilename = "../share/knife_detector.svm";
-  object_detector< scan_fhog_pyramid< pyramid_down<6> > > detector;
+int main(int argc, char **argv) {
+  const string detectorFilename = "../share/fakegun.svm";
+  object_detector<scan_fhog_pyramid<pyramid_down<6>>> detector;
   deserialize(detectorFilename) >> detector;
   // image_window hogwin(draw_fhog(detector), "Learned fHOG detector");
 
@@ -27,8 +27,8 @@ int main(int argc, char** argv) {
     throw runtime_error("Invalid arguments.");
   }
 
-  std::vector<file> files = get_files_in_directory_tree(argv[1],
-                                                        match_ending(".jpg"));
+  std::vector<file> files =
+      get_files_in_directory_tree(argv[1], match_ending(".jpg"));
   std::sort(files.begin(), files.end());
   if (files.size() == 0) {
     throw runtime_error("No images found.");
@@ -41,36 +41,14 @@ int main(int argc, char** argv) {
 
   unsigned long fid;
   for (fid = 0; fid < files.size() && !detected; ++fid) {
+    std::cout << "Processing file: " << files[fid] << std::endl;
     load_image(img, files[fid]);
     win.set_image(img);
 
-    array2d<unsigned char> imgvm, imghm, imgvhm;
-    flip_image_up_down(img, imghm);
-    flip_image_left_right(img, imgvm);
-    flip_image_left_right(imghm, imgvhm);
-
     std::vector<rectangle> dets = detector(img);
-    std::vector<rectangle> detshm = detector(imghm);
-    std::vector<rectangle> detsvm = detector(imgvm);
-    std::vector<rectangle> detsvhm = detector(imgvhm);
-
-    detected = !(dets.empty() && detshm.empty() && detsvm.empty()
-               && detsvhm.empty());
+    detected = !dets.empty();
     if (!dets.empty()) {
       object_roi = drectangle(dets.front());
-    }
-    else if (!detsvm.empty()) {
-      rectangle r = detsvm.front();
-      object_roi = drectangle(rectangle(r.right(), r.top(),
-                                        r.left(), r.bottom()));
-    } else if (!detshm.empty()) {
-      rectangle r = detshm.front();
-      object_roi = drectangle(rectangle(r.left(), r.bottom(),
-                                        r.right(), r.top()));
-    } else if (!detsvhm.empty()) {
-      rectangle r = detsvhm.front();
-      object_roi = drectangle(rectangle(r.right(), r.bottom(),
-                                        r.left(), r.top()));
     }
   }
 
